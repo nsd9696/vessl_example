0ce68d64dfd0f38c82b321ff1555a4aeaeecd952
nsd9696 <nsd26534613@gmail.com>
2023-10-30 13:57:50 +0900

    change path

--- test_dataset/main.py
+++ test_dataset/main.py
@@ -1,7 +1,5 @@
 import vessl
-import shutil
 
 vessl.init()
-data_path = "/root/examples/test_datast/logo.ico"
-shutil.copy(data_path, "/root/data/logo.ico")
-# vessl.upload_dataset_volume_file(dataset_name="VSSLLMFLOW", source_path=data_path, dest_path="/", organization_name="lucas")
+data_path = "logo.ico"
+vessl.upload_dataset_volume_file(dataset_name="VSSLLMFLOW", source_path=data_path, dest_path="/", organization_name="lucas")
