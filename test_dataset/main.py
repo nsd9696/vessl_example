3726432df5631f01ab4b96651638cd8ca71253e6
nsd9696 <nsd26534613@gmail.com>
2023-10-30 13:51:14 +0900

    dataset shutil copy

--- test_dataset/main.py
+++ test_dataset/main.py
@@ -1,5 +1,7 @@
 import vessl
+import shutil
 
 vessl.init()
 data_path = "/root/examples/test_datast/logo.ico"
-vessl.upload_dataset_volume_file(dataset_name="VSSLLMFLOW", source_path=data_path, dest_path="/", organization_name="lucas")
+shutil.copy(data_path, "/root/data/logo.ico")
+# vessl.upload_dataset_volume_file(dataset_name="VSSLLMFLOW", source_path=data_path, dest_path="/", organization_name="lucas")
